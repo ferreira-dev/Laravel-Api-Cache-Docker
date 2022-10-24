ee96b099bedbfe43051cb865b09cd80c1147864e
Ferreira Dev Estudos <fabroide@gmail.com>
2022-10-24 16:11:43 -0300

    [new] modulo apis da ead

--- Projeto/routes/api.php
+++ Projeto/routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\{
+    CourseController
+};
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/courses', [CourseController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function(){
+    return response()->json(['message' => 'ok']);
 });
